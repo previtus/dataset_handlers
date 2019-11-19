7a4e8fe7c680198c7d35c44d857a92ed4a0fd2c4
previtus <previtus@gmail.com>
2019-11-19 18:01:02 +0000

    delete old files

--- raster_fairy_0test.py
+++
@@ -1,5 +0,0 @@
-import rasterfairy
-
-#xy should be a numpy array with a shape (number of points,2)
-grid_xy = rasterfairy.transformPointCloud2D(xy)
-#grid_xy will contain the points in the same order but aligned to a grid
