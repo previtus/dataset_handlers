c0a355dd278f664692b445b6f9b5678190987fda
previtus <previtus@gmail.com>
2019-11-27 16:55:09 +0000

    clean a bit

--- raster_fairy_1prep.py
+++ raster_fairy_1prep.py
@@ -35,9 +35,6 @@ def get_image(path):
     x = preprocess_input(x)
     return img, x
 
-import PIL.Image
-from IPython.display import clear_output, Image, display, HTML
-
 feat_extractor = Model(inputs=model.input, outputs=model.get_layer("fc2").output)
 feat_extractor.summary()
 
@@ -62,36 +59,5 @@ pca = PCA(n_components=n_comp)
 pca.fit(features)
 pca_features = pca.transform(features)
 
-def get_closest_images(query_image_idx, num_results=5):
-    distances = [ distance.euclidean(pca_features[query_image_idx], feat) for feat in pca_features ]
-    idx_closest = sorted(range(len(distances)), key=lambda k: distances[k])[1:num_results+1]
-    return idx_closest
-
-def get_concatenated_images(indexes, thumb_height):
-    thumbs = []
-    for idx in indexes:
-        img = image.load_img(images[idx])
-        img = img.resize((int(img.width * thumb_height / img.height), thumb_height))
-        thumbs.append(img)
-    concat_image = np.concatenate([np.asarray(t) for t in thumbs], axis=1)
-    return concat_image
-
-"""
-# do a query on a random image
-query_image_idx = int(len(images) * random.random())
-idx_closest = get_closest_images(query_image_idx)
-query_image = get_concatenated_images([query_image_idx], 300)
-results_image = get_concatenated_images(idx_closest, 200)
-
-# display the query image
-matplotlib.pyplot.figure(figsize = (5,5))
-imshow(query_image)
-matplotlib.pyplot.title("query image (%d)" % query_image_idx)
-
-# display the resulting images
-matplotlib.pyplot.figure(figsize = (16,12))
-imshow(results_image)
-matplotlib.pyplot.title("result images")
-"""
 
 pickle.dump([images, pca_features], open(name_result_dumpfile, 'wb'))
